c9a8a2b6071c6737b9f4855e42b838a1ab0a00d5
LAKIR0 <115772602+LAKIR0@users.noreply.github.com>
2022-10-14 13:19:21 +0530

    Add files via upload

---
+++ 14oct/DataTypeDemo.java
@@ -0,0 +1,16 @@
+class DataTypeDemo
+{
+	public static void main(String args[])
+	{
+		int a =1;
+		float b=1.3f;
+		boolean pass = true;
+		char c ='h';
+		System.out.println("examples of variables");
+		System.out.println("int = "+a);
+		System.out.println("float = "+b);
+		System.out.println("boolean = "+pass);
+		System.out.println("char = "+c);
+	}
+}
+	
